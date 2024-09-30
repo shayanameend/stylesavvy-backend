2f81dbe9a1f69b31054365ab30d6da7e8503be08
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-10-01 04:32:28 +0500

    Refactor error handling in env validation
    Replaced direct error handling in env.ts with a new handleZodErrors function to streamline and label the errors. Updated envSchema validation rules for HOST and PORT to be more flexible.

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,29 +1,19 @@
 import * as zod from "zod";
 import {NODE_ENV} from "./types";
 import * as process from "node:process";
-import {formatZodErrors} from "./zod";
+import {handleZodErrors} from "./zod";
 
 const envSchema = zod.object({
   NODE_ENV: zod.enum([NODE_ENV.DEVELOPMENT, NODE_ENV.TESTING, NODE_ENV.PRODUCTION]),
-  HOST: zod.string().url(),
-  PORT: zod.coerce.number().min(4).max(4),
+  HOST: zod.string(),
+  PORT: zod.coerce.number().min(1000).max(999999),
 })
 
 
 const validatedEnv = envSchema.safeParse(process.env)
 
 if (validatedEnv.error) {
-  const error = validatedEnv.error
-
-  if (error instanceof zod.ZodError) {
-    console.error(formatZodErrors(error))
-
-    process.exit(0)
-  }
-
-  console.error(JSON.stringify(error))
-
-  process.exit(1)
+  handleZodErrors(validatedEnv.error, "Env")
 }
 
 export const env = validatedEnv.data
--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -1,6 +1,17 @@
 import * as zod from "zod"
 
 export function formatZodErrors(error: zod.ZodError) {
-  // @ts-ignore
-  return error.errors.concat().map(item => `Received ${JSON.stringify(item.received)}, instead of ${JSON.stringify(item.expected)} at ${item.path[0]}`)
+  return error.errors.concat().map(item => `${item.path[0]}: ${item.message}`)
+}
+
+export function handleZodErrors(error: zod.ZodError, label?: string) {
+  if (error instanceof zod.ZodError) {
+    console.error(label ? `${label} Errors:` : "Errors:", formatZodErrors(error))
+
+    process.exit(0)
+  }
+
+  console.error(JSON.stringify(error))
+
+  process.exit(1)
 }
