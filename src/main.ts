0dae00d38ac681467fba8633e06a15a0e45b3755
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-10-09 14:14:41 +0500

    Refactor package.json to add express and @types/express dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { env } from './lib/env';
 import { NODE_ENV } from './lib/types';
+import { default as express } from 'express';
+import { default as http } from 'node:http';
 
-console.log(
-  `${env.NODE_ENV === NODE_ENV.PRODUCTION ? 'https' : 'http'}://${env.HOST}:${env.PORT}`,
-);
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const server = http.createServer(app);
+
+server.listen(env.PORT, env.HOST, () => {
+  console.log(
+    `${env.NODE_ENV === NODE_ENV.PRODUCTION ? 'https' : 'http'}://${env.HOST}:${env.PORT}`,
+  );
+});
